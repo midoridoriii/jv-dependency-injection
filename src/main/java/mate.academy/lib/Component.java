9f08c09f9932732139985124198b38af83f086fc
midoridoriii <valissa069@gmail.com>
2026-02-17 07:45:29 +0200

    made annotations visible for reflection in Component

--- src/main/java/mate.academy/lib/Component.java
+++ src/main/java/mate.academy/lib/Component.java
@@ -1,5 +1,12 @@
 package mate.academy.lib;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)фа
 public @interface Component {
 
 }
